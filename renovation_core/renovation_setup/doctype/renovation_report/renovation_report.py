90bdc887083c55a17a72b8bb7f035d530bc52de7
Mainul Islam <mainulkhan94@gmail.com>
2019-02-27 16:00:44 +0600

    Update Removation Report Filter System

--- renovation_core/renovation_setup/doctype/renovation_report/renovation_report.py
+++ renovation_core/renovation_setup/doctype/renovation_report/renovation_report.py
@@ -5,6 +5,20 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.desk.query_report import get_script
 
 class RenovationReport(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_defaults_filters(report):
+	report = frappe.get_doc("Report", report)
+	if report.report_type in ('Query Report', 'Script Report'):
+		script = get_script(report.name).get('script')
+	else:
+		script = [field for field in frappe.get_meta(report.ref_doctype).fields if field.get('in_standard_filter')]
+	return {
+		"is_standard": not report.report_type in ('Query Report', 'Script Report'),
+		"script":script
+	}
