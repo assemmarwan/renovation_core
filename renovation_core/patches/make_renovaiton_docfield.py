b772e3e6d5abacc1c94903f04027e7f0d6f9821a
Mainul Islam <mainulkhan94@gmail.com>
2019-04-11 16:41:29 +0600

    Fixed Make Field Patch

--- renovation_core/patches/make_renovaiton_docfield.py
+++ renovation_core/patches/make_renovaiton_docfield.py
@@ -1,1 +1,6 @@
-from renovation_core.renovation_core.doctype.renovation_docfield.renovation_docfield import add_all_reqd_table_fields as execute
+import frappe
+from renovation_core.renovation_core.doctype.renovation_docfield.renovation_docfield import add_all_reqd_table_fields
+
+def execute():
+    frappe.reload_doc('renovation_core', 'doctype', 'renovation_docfield', force=True)
+    add_all_reqd_table_fields()
