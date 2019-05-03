6b9066ef9b6802087e368d45d7cce3667fa557c4
Fahim <faztp12@gmail.com>
2019-05-03 22:47:41 +0200

    ReApply All button in Renovation Image Settings

--- renovation_core/renovation_setup/doctype/renovation_image_settings/renovation_image_settings.js
+++ renovation_core/renovation_setup/doctype/renovation_image_settings/renovation_image_settings.js
@@ -3,6 +3,13 @@
 
 frappe.ui.form.on('Renovation Image Settings', {
 	refresh: function(frm) {
-
+		frm.add_custom_button(__('Regenerate All'), function() {
+			frappe.confirm('Re applying watermarks and generating thumbnails for them is a long process. Are you sure to proceed ?', function() {
+				frappe.call({
+					method: 'reapply_all',
+					doc: frm.doc
+				})
+			})
+		})
 	}
 });
--- renovation_core/renovation_setup/doctype/renovation_image_settings/renovation_image_settings.py
+++ renovation_core/renovation_setup/doctype/renovation_image_settings/renovation_image_settings.py
@@ -7,9 +7,16 @@ import frappe
 from frappe.model.document import Document
 
 class RenovationImageSettings(Document):
-	
-	def regenerate_all_thumbs(self):
-		from renovation_core.utils.images import regenerate_all_thumbnails
-		frappe.enqueue(method=regenerate_all_thumbnails, queue="long", job_name="regenerate_thumbnails")
-		
-		frappe.msgprint("The thumbnails will be regenerated in the background. Please continue")
+
+	def reapply_all(self):
+		from frappe.utils.background_jobs import enqueue, get_jobs
+		from frappe.core.page.background_jobs.background_jobs import get_info as get_running_jobs
+
+		job_name = "reapply_all_image_watermarks"
+
+		if job_name not in [job.get("job_name") for job in get_running_jobs()]:
+			from renovation_core.utils.images import regenerate_all_thumbnails, reapply_all_watermarks
+			enqueue(method=reapply_all_watermarks, queue="long", job_name=job_name, timeout=7200) # 2hr
+			frappe.msgprint("The thumbnails will be regenerated in the background. Please handle CDN cache if any.")
+		else:
+			frappe.msgprint("A reapply request is already in process, please try again later")
