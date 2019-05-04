a2f7c22144854ef245b0a00e00ba2c97b85dc85c
Mainul Islam <mainulkhan94@gmail.com>
2019-05-04 10:55:15 +0600

    Added Files to Generate Docs

--- renovation_core/config/docs.py
+++ renovation_core/config/docs.py
@@ -2,10 +2,10 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/renovation_core"
-# docs_base_url = "https://[org_name].github.io/renovation_core"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/MalikZu/renovation_core.git"
+docs_base_url = "/docs/renovation_core"
+headline = "App that does everything"
+sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
 	context.brand_html = "Renovation Core"
