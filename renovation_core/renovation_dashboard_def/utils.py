37f00395dc9248702582226fb335a2c3d2434684
Fahim <faztp12@gmail.com>
2019-06-30 08:35:07 +0000

    Do not throw for dashboard cache related errors

--- renovation_core/renovation_dashboard_def/utils.py
+++ renovation_core/renovation_dashboard_def/utils.py
@@ -107,9 +107,12 @@ def clear_cache_on_doc_events(doc, method):
 
 
 def get_dashboards_for_clear_cahe(doctype):
-    cache_key = '_{}_puge_cache'.format(doctype)
-    if frappe.cache().hget('dashboard', cache_key):
-        return frappe.cache().hget('dashboard', cache_key)
-    data = [x.parent for x in frappe.get_all('Renovation Purge Cache', {'link_doctype': doctype, 'parenttype': 'Renovation Dashboard'}, 'parent')]
-    frappe.cache().hset('dashboard', cache_key, data)
-    return data
+    try:
+        cache_key = '_{}_puge_cache'.format(doctype)
+        if frappe.cache().hget('dashboard', cache_key):
+            return frappe.cache().hget('dashboard', cache_key)
+        data = [x.parent for x in frappe.get_all('Renovation Purge Cache', {'link_doctype': doctype, 'parenttype': 'Renovation Dashboard'}, 'parent')]
+        frappe.cache().hset('dashboard', cache_key, data)
+        return data
+    except:
+        return []
